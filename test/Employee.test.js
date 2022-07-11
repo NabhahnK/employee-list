1cb5d33a88dcf39a493a327e970ac59a7533b3b7
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-07-11 13:31:47 -0700

    created first test for Employee class

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,2 +1,24 @@
-const employee = require("../lib/Employee");
+const fs = require("fs");
+const Employee = require("../lib/Employee");
+jest.mock("fs");
 
+
+describe("Employee", () => {
+    describe("buildEmployee", () => {
+        it("Should build an Employee with three keys", () => {
+            const name = "Bob";
+            const id = 7;
+            const email = "Test@email.com";
+            let count = 0;
+
+            const testE = new Employee(name, id, email);
+            for(let properties in testE) {
+                count = count + 1;
+            }
+
+            expect(count).toEqual(3);
+        })
+    })
+
+    // describe();
+})

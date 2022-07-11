e8b2f826fa0d8bbdb29424fc735d06ddafef3fa4
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-07-11 14:17:59 -0700

    made manager questions and a function if they wanted to add more

--- index.js
+++ index.js
@@ -1,6 +1,56 @@
 const fs = require("fs");
 const inquirer = require('inquirer');
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+let team = [];
+let continueBuilding = true;
 
-const dude = new Manager("bob", 18, "ablah@gmailk", 1);
-console.log(dude.getRole())
+function checkBuildStatus() {
+    inquirer
+        .prompt([
+            {
+                name: "moreE",
+                type: "confirm",
+                message: "Do you have another employee?"
+            }
+        ])
+        .then((answer) => {
+            if(answer) {
+                checkType();
+            } else {
+                return
+            }
+        })
+}
+
+inquirer
+    .prompt([
+        {
+            name: "managerName",
+            type: "input",
+            message: "What is your managers name?",
+        },
+        {
+            name: "managerID",
+            type: "number",
+            message: "What is their ID number?"
+        },
+        {
+            name: "managerEmail",
+            type: "input",
+            message: "What is their Email?"
+        },
+        {
+            name: "managerOfficeNumber",
+            type: "number",
+            message: "What is their office number?"
+        }
+    ])
+    .then((...answers) => {
+        team.push(answers);
+        console.log(team);
+        checkBuildStatus();
+    });
+
